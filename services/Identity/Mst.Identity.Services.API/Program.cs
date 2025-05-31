a8ff3767d646fc32ae4b47f1a20943d1a0e61718
Deepakkushwaha01 <deepakkushwaha95820@gmail.com>
2025-05-31 21:07:50 +0530

    [Remove]: bin and obj ye build se bante hai

--- services/Identity/Mst.Identity.Services.API/Program.cs
+++ services/Identity/Mst.Identity.Services.API/Program.cs
@@ -1,41 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Mst.Identity.Services.API
 {
-    app.MapOpenApi();
-}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                RunApplication(BuildApplication(WebApplication.CreateBuilder(args)));
+            }
+            catch (Exception)
+            {
 
-app.UseHttpsRedirection();
+                throw;
+            }
+        }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+        public static WebApplication BuildApplication(WebApplicationBuilder builder)
+        {
+            return builder.Build();
+        }
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public static void RunApplication(WebApplication app)
+        {
+            app.MapGet("/", () => Results.Ok("Welcome to Identity Service API"));
+            app.Run();
+        }
+    }
 }
