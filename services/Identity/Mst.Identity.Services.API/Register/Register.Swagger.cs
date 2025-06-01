c8f30cee170a84b8cdbd7062938ce3bfccbfca57
Deepakkushwaha01 <deepakkushwaha95820@gmail.com>
2025-06-01 11:31:48 +0530

    [Add]: Swagger Add with configuration and demo model and controllers

---
+++ services/Identity/Mst.Identity.Services.API/Register/Register.Swagger.cs
@@ -0,0 +1,54 @@
+using Microsoft.OpenApi.Models;
+using Mst.Identity.Services.API.Settings;
+
+namespace Mst.Identity.Services.API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEndpointsApiExplorer();
+            SwaggerSettings swaggerSettings = new(configuration);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(swaggerSettings.Version, new OpenApiInfo
+                {
+                    Title = swaggerSettings.ApplicationName,
+                    Version = swaggerSettings.Version,
+                    Description = swaggerSettings.Description
+                });
+
+                // Add GUID schema handling
+                options.MapType<Guid>(() => new OpenApiSchema { Type = "string", Format = "uuid" });
+
+                // If using XML comments (optional)
+                var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                if (File.Exists(xmlPath))
+                {
+                    options.IncludeXmlComments(xmlPath);
+                }
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            SwaggerSettings swaggerSettings = new(configuration);
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = swaggerSettings.RouteTemplate;
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(swaggerSettings.Version1_0_JsonEndpointUrl, swaggerSettings.Version1_0_Name);
+                c.RoutePrefix = swaggerSettings.RoutePrefix; // Access at route
+            });
+
+            return app;
+        }
+    }
+}
